c42c792fe78b823e9268f020114092a4e16a4c38
muharamrizal2 <muharamrizal2@gmail.com>
2021-07-15 21:07:03 +0700

    CI development Set-up

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,55 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import express from 'express';
+import { Request, Response } from 'express';
+
+import * as jwt from 'jsonwebtoken';
+
+import { authenticateTokenAdmin, generateAccessToken } from './middleware';
 
 const app = express();
 dotenv.config();
 app.use(bodyParser.json());
 app.use(cors());
-const port: number = 8080;
+
+const port = 8080;
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan.default('dev'));
 }
+
+app.post('/', (req: Request, res: Response) => {
+  const { username, role } = req.body;
+  const token = jwt.sign(
+    { username, role },
+    process.env.TOKEN_SECRET as string,
+    {
+      expiresIn: '1h',
+    },
+  );
+  res.status(200).json({ token });
+});
+
+app.get('/', async (req: Request, res: Response) => {
+  const { token } = req.body;
+  const isi = await jwt.verify(
+    token,
+    process.env.TOKEN_SECRET as string,
+    (err: any, decoded: jwt.JwtPayload | undefined) => {
+      if (err) return res.sendStatus(403);
+      // if (decoded.role !== 'Admin' && decoded.role !== 'Super Admin') {
+      //   return res.sendStatus(403);
+      // }
+      console.log(decoded);
+      if (decoded) console.log(decoded.username);
+    },
+  );
+  res.status(200).json({ isi });
+});
 
 const server = app.listen({ port }, () =>
   console.log(
-    `🚀 Server ready at: http://localhost:${port} \n⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`
-  )
+    `🚀 Server ready at: http://localhost:${port} \n⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`,
+  ),
 );
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
-const dotenv = require('dotenv');
+import * as dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -33,21 +33,23 @@ export const authenticateTokenAdmin = (
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
   if (token == null) return res.sendStatus(401);
+  // jwt.verify(token, process.env.TOKEN_SECRET as string);
   jwt.verify(
     token,
     process.env.TOKEN_SECRET as string,
-    (err: any, decoded: DecodedType) => {
+    (err: any, decoded: jwt.JwtPayload | undefined) => {
       if (err) return res.sendStatus(403);
-      if (decoded.role !== 'Admin' && decoded.role !== 'Super Admin') {
-        return res.sendStatus(403);
-      }
+      // if (decoded.role !== 'Admin' && decoded.role !== 'Super Admin') {
+      //   return res.sendStatus(403);
+      // }
+      console.log(decoded);
       next();
     }
   );
 };
 
 export const generateAccessToken = (username: string, role: string) => {
-  return jwt.sign({ username, role }, process.env.TOKEN_SECRET, {
+  return jwt.sign({ username, role }, process.env.TOKEN_SECRET as string, {
     expiresIn: '1h',
   });
 };
